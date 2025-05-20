c3670d80ee6de8eeab19bab85e66d73612fee734
Kishore Sai <kishore.sai207@gmail.com>
2025-05-21 00:45:56 +0530

    updated package file

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,19 +1,19 @@
 import './index.css';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './App';
 import { AuthProvider } from './contexts/AuthContext';
 import { TaskProvider } from './contexts/TaskContext';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <AuthProvider>
         <TaskProvider>
           <App />
         </TaskProvider>
       </AuthProvider>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 );
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: 'Employee-Task-Tracker',
+  base: '/Employee-Task-Tracker/',
   plugins: [react()],
 });
 
