a8bd6c930091fe39ab2cf046979ace13df80ed8b
Kishore Sai <kishore.sai207@gmail.com>
2025-05-21 01:47:25 +0530

    config updated

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite';
+/*import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
@@ -8,4 +8,4 @@ export default defineConfig({
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
-});
+});*/
